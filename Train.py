3987f5b680f3866dd3ceea85686bac83bafa82db
nightwind__ <115386737+nightwind0623@users.noreply.github.com>
2024-04-30 22:10:22 +0800

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # model = YOLO("./ModelWeights/yolov8n.pt")  # load a pretrained YOLOv8n model
-    # # model.train(data="C:\\Users\\kevin\\OneDrive\\桌面\\helloworld\\py\\csgo\\data.yaml", epochs=400, amp=False)  # train the model
-    # model.train(data="C:\\Users\\kevin\\OneDrive\\桌面\\helloworld\\py\\datasets\\F8bGaOWEMK\\data.yaml", epochs=400, amp=False, seed=1)  # train the model
-    # model.val()  # evaluate model performance on the validation set
-    # model.export(format="engine")  # export the model to ONNX format
-    
-    model = YOLO("./runs/detect/train4/weights/last.pt")
-    model.train(resume=True)
+    model = YOLO("./ModelWeights/yolov8n.pt")
+    model.train(data="data.yaml", epochs=400, amp=False)
+    model.val() 
     model.export(format="engine")
+    
+    # model = YOLO("./runs/detect/train4/weights/last.pt")
+    # model.train(resume=True)
+    # model.export(format="engine")
