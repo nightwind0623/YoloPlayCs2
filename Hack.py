f3415462951f85422cc9e4baa64e323cfd58460e
nightwind <kevin.hongdragon@gmail.com>
2024-05-25 12:04:18 +0800

    modified:   Hack.py

--- Hack.py
+++ Hack.py
@@ -11,8 +11,6 @@ def screen_capture(screen):
     screen_img = ImageGrab.grab(bbox=screen_box)
     return screen_img
 
-def calc_mid(a, b):
-    return int((a + b) / 2)
 
 model = YOLO('./ModelWeights/F3.engine')
 gameScreen = gw.getWindowsWithTitle('Counter-Strike 2')[0]
@@ -21,13 +19,13 @@ while True:
 
     if keyboard.is_pressed("p") == True:
         results = model(screen_capture(gameScreen), conf=0.7)
-        enemy_closest = head_closest = [float('inf'), 0, 0] #index: 0->distance, 1->dx, 2->dy
+        enemy_closest = head_closest = [float('inf'), 0, 0] #索引：目標距畫面中心距離、目標x座標、目標y座標
         
         for box in results[0].boxes:
-            class_index = int(box.cls)
+            class_index = int(box.cls) #0：敵人頭部　1：敵人身體
             
-            mouse_x, mouse_y = pyautogui.position()[0], pyautogui.position()[1]
-            x_mid, y_mid = int((box.xyxy[0][0] + box.xyxy[0][2]) / 2), int((box.xyxy[0][1]), int(box.xyxy[0][3]) / 2)
+            mouse_x, mouse_y = pyautogui.position()[0], pyautogui.position()[1] #滑鼠座標
+            x_mid, y_mid = int((box.xyxy[0][0] + box.xyxy[0][2]) / 2), int((box.xyxy[0][1]), int(box.xyxy[0][3]) / 2) #計算目標中心點
             distance = (x_mid - mouse_x)**2 + (y_mid - mouse_y)**2
             
             if class_index == 0 and distance < head_closest[0]: 
@@ -36,7 +34,7 @@ while True:
                 enemy_closest = [distance, x_mid, y_mid]
                 
         if head_closest != [float('inf'), 0, 0]:
-            Mouse.move(head_closest[1] - mouse_x, head_closest[2] - mouse_y, False)
+            Mouse.move(head_closest[1] - mouse_x, head_closest[2] - mouse_y, False) #參數：移動x座標、移動y座標、是否使用絕對定位
         elif enemy_closest != [float('inf'), 0, 0]:
             Mouse.move(enemy_closest[1] - mouse_x, enemy_closest[2] - mouse_y, False)
             
