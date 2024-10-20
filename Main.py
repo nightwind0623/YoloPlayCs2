5be9e04ecc4f4c2b6204f932e87c70c757741cb7
nightwind__ <kevin.hongdargon@gmail.com>
2024-10-20 12:20:05 +0800

    deleted:    .history/Hack_20240629192334.py 	deleted:    .history/Hack_20240714140604.py 	deleted:    .history/Hack_20240714140608.py 	deleted:    .history/ModelWeights/ValidateData_20240709192720.txt 	deleted:    .history/ModelWeights/ValidateData_20240709192850.txt 	deleted:    .history/README_20240629192334.md 	deleted:    .history/README_20240709202917.md 	deleted:    .history/README_20240709202919.md 	deleted:    .history/README_20240709202923.md 	deleted:    .history/Train_20240709192306.py 	deleted:    .history/Train_20240709202344.py 	deleted:    Hack.py renamed:    .history/Hack_20240714140637.py -> Main.py 	deleted:    run.ipynb

--- Main.py
+++ Main.py
@@ -11,27 +11,25 @@ def screen_capture(screen):
     screen_img = ImageGrab.grab(bbox=screen_box)
     return screen_img
 
-
 model = YOLOv10('./ModelWeights/V10mF1.engine')
 gameScreen = gw.getWindowsWithTitle('Counter-Strike 2')[0]
 
 while True:
-
+    
     if keyboard.is_pressed("p") == True:
         results = model(screen_capture(gameScreen), conf=0.7)
         enemy_closest = head_closest = [float('inf'), 0, 0] #索引：目標距畫面中心距離、目標x座標、目標y座標
         
         for box in results[0].boxes:
             class_index = int(box.cls) #0：敵人頭部　1：敵人身體
-            
             mouse_x, mouse_y = pyautogui.position()[0], pyautogui.position()[1] #滑鼠座標
-            x_mid, y_mid = int((box.xyxy[0][0] + box.xyxy[0][2]) / 2), int((box.xyxy[0][1]), int(box.xyxy[0][3]) / 2) #計算目標中心點
+            x_mid, y_mid = int((box.xyxy[0][0] + box.xyxy[0][2]) / 2), int((box.xyxy[0][1] + box.xyxy[0][3]) / 2) #計算目標中心點
             distance = (x_mid - mouse_x)**2 + (y_mid - mouse_y)**2
             
             if class_index == 0 and distance < head_closest[0]: 
                 head_closest = [distance, x_mid, y_mid]
             elif class_index == 1 and distance < enemy_closest[0]: 
-                enemy_closest = [distance, x_mid, y_mid]
+                enemy_closest = [distance, x_mid, y_mid]  
                 
         if head_closest != [float('inf'), 0, 0]:
             Mouse.move(head_closest[1] - mouse_x, head_closest[2] - mouse_y, False) #參數：移動x座標、移動y座標、是否使用絕對定位
